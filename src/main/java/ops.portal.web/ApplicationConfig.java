27d78aa26046e08ce881b669d8a334fdcbb14b8a
Serghei Scobici <sergey.scobich@gmail.com>
2016-11-23 23:55:21 +0200

    Initial commit

---
+++ src/main/java/ops.portal.web/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package ops.portal.web;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class ApplicationConfig {
+}
