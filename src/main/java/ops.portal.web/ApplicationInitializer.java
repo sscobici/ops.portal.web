0fb2ded1614712c8a8b9791e79cf85929a46b6e6
Serghei Scobici <sergey.scobich@gmail.com>
2016-11-30 00:25:50 +0200

    mvc 1

---
+++ src/main/java/ops.portal.web/ApplicationInitializer.java
@@ -0,0 +1,20 @@
+package ops.portal.web;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+    public void onStartup(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(WebConfig.class);
+
+        ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
+}
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -2,6 +2,6 @@
   <head><title>Example :: Spring Application</title></head>
   <body>
     <h1>Example - Spring Application</h1>
-    <p>This is my test.</p>
+    <p>This is my VIEW.</p>
   </body>
 </html>
